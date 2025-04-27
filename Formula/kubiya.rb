1ca53ea1fcf8a868d596fa1ed7edff4e0addef95
GitHub Action <action@github.com>
2025-04-27 12:51:21 +0000

    Update kubiya formula to version

--- Formula/kubiya.rb
+++ Formula/kubiya.rb
@@ -1,9 +1,13 @@
 class Kubiya < Formula
   desc "Command line interface for Kubiya AI platform"
   homepage "https://github.com/kubiyabot/cli"
-  url "https://github.com/kubiyabot/cli/releases/download/0.0.0-dev.8bfff5e/kubiya-cli-darwin-amd64"
+  url "https://github.com/kubiyabot/cli/releases/download/0.0.0-dev.d203fc9/kubiya-cli-darwin-amd64"
   sha256 "92ca2e8a76c0c503530be074b054fd533041f682a6717d2333ebd103cef8204d"
-  version "0.0.0-dev.8bfff5e"
+  version "0.0.0-dev.d203fc9"
+  
+  # Specify platform-specific dependencies
+  depends_on :macos
+  depends_on "go" => :build
   
   def install
     bin.install "kubiya-cli-darwin-amd64" => "kubiya"
@@ -11,5 +15,15 @@ class Kubiya < Formula
   
   test do
     system "#{bin}/kubiya", "--version"
+  end
+  
+  # Add caveats for users
+  def caveats
+    <<~EOS
+      kubiya has been installed as "kubiya".
+      
+      To get started, run:
+        kubiya --help
+    EOS
   end
 end
