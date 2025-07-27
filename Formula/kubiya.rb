a6d16059dc185edd1905f40af700ff12688a8e59
GitHub Action <action@github.com>
2025-07-27 23:39:36 +0000

    Update kubiya formula to version

--- Formula/kubiya.rb
+++ Formula/kubiya.rb
@@ -1,9 +1,9 @@
 class Kubiya < Formula
   desc "Command line interface for Kubiya AI platform"
   homepage "https://github.com/kubiyabot/cli"
-  url "https://github.com/kubiyabot/cli/releases/download/v2.5.0/kubiya-cli-darwin-amd64"
-  sha256 "7ff0cd9aa2aecc67e03ef56afe0eabf7bcdde70e9e1680e992ace1cec0534aa7"
-  version "2.5.0"
+  url "https://github.com/kubiyabot/cli/releases/download/v2.5.1/kubiya-cli-darwin-amd64"
+  sha256 "748ce346b2c0cd1d7e725f4de8a6a139ec5492c285aad937877b7f32c6fa0e15"
+  version "2.5.1"
   
   def install
     bin.install "kubiya-cli-darwin-amd64" => "kubiya"
